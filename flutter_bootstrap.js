d64d9740ae52f3f088481a0bce545e6e8fcffb03
Precious Umeha <70241283+pumeha@users.noreply.github.com>
2025-03-03 09:42:00 +0100

    3/3/25
    added UX to the dashboardPage

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3910744537"
+    serviceWorkerVersion: "3337561724"
   }
 });
