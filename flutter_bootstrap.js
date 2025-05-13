661d34c71e8cbd845424122326c083e4b4bf0430
Precious Umeha <70241283+pumeha@users.noreply.github.com>
2025-05-13 16:51:51 +0100

    modify codes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2469914806"
+    serviceWorkerVersion: "3323998996"
   }
 });
