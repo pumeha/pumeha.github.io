9252510dc399792457006aaf59a425ccfbfa8fb2
Precious Umeha <70241283+pumeha@users.noreply.github.com>
2025-09-29 06:43:57 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "694395811"
+    serviceWorkerVersion: "368732474"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,10 +28,10 @@ const RESOURCES = {"404.html": "327d334fcbf901c400eddc188d0d3668",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "f791f85711c53614f26407efe282c53c",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "c89fca2328d6ab4d61ad7259384e561b",
+"flutter_bootstrap.js": "065536f0f1797e122c9943fb5df54f96",
 "index.html": "ae1514fab026cfcce1f5043107f83598",
 "/": "ae1514fab026cfcce1f5043107f83598",
-"main.dart.js": "20b3b56bd1c23905a631da9fcb30a934",
+"main.dart.js": "803a7e91ae2aadaa177073e67a74ee9a",
 "manifest.json": "e17e36181726c32bb9a6fecfffdf6eab",
 "version.json": "36c16fbd878964ec918dcacb741890ae"};
 // The application shell files that are downloaded before a service worker can
